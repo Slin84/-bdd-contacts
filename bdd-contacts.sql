5a9152df5ae7d485667bf8ce05707d1c1872e1fa
Slin84 <nils84210@hotmail.fr>
2023-11-23 10:00:07 +0100

    ajout tel

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -1,14 +1,17 @@
-create database if not exists essai;
+create database if not exists BDD;
 show databases;
-use essai;
+use BDD;
 drop table if exists contacts;
 create table if not exists contacts(ID int, Nom varchar(70),
 Prenom varchar(70),
 Date_de_naissance date,
 Sexe ENUM('Homme','Femme','Autre'),
 Adresse text,
-Cp varchar(10)
-Ville varchar(70)
+Cp varchar(10),
+Ville varchar(70),
 Pays_Iso3 varchar(3));
-show tables;
+drop table if exists telephone;
+create table if not exists telephone(ID int, ID_contact int, numero varchar(50), type ENUM('Portable','Fixe'));
+Show tables;
 Describe contacts;
+Describe telephone;
