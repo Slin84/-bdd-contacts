94e86120e57d5c5d3c97e98611722029a4c73ae0
Slin84 <nils84210@hotmail.fr>
2023-11-23 09:34:50 +0100

    ajoute contacts

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -1,1 +1,14 @@
-test
+create database if not exists essai;
+show databases;
+use essai;
+drop table if exists contacts;
+create table if not exists contacts(ID int, Nom varchar(70),
+Prenom varchar(70),
+Date_de_naissance date,
+Sexe ENUM('Homme','Femme','Autre'),
+Adresse text,
+Cp varchar(10)
+Ville varchar(70)
+Pays_Iso3 varchar(3));
+show tables;
+Describe contacts;
